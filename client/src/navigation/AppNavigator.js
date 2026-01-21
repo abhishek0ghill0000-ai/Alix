eb3fb8ac7012c86659bd6049e5aac1545430e913
Abhi <abhishek0ghill0000@gmail.com>
2026-01-21 17:53:14 +0530

    Update AppNavigator.js

--- client/src/navigation/AppNavigator.js
+++ client/src/navigation/AppNavigator.js
@@ -2,19 +2,32 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { Image } from 'react-native';
 
-// Placeholder Screens
+const Tab = createBottomTabNavigator();
+
+// Screens import karenge
 import ChatScreen from '../screens/ChatScreen';
 import SearchScreen from '../screens/SearchScreen';
 import VideoCallScreen from '../screens/VideoCallScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 
-const Tab = createBottomTabNavigator();
-
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Chat">
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            let iconName;
+            if (route.name === 'Search') iconName = focused ? 'nav/search-active.png' : 'nav/search.png';
+            if (route.name === 'VideoCall') iconName = focused ? 'nav/video-active.png' : 'nav/video.png';
+            if (route.name === 'Chat') iconName = focused ? 'nav/chat-active.png' : 'nav/chat.png';
+            if (route.name === 'Profile') iconName = focused ? 'nav/profile-active.png' : 'nav/profile.png';
+            
+            return <Image source={require(`../assets/icons/nav/${iconName}`)} style={{width: 24, height: 24}} />;
+          }
+        })}
+      >
         <Tab.Screen name="Search" component={SearchScreen} />
         <Tab.Screen name="VideoCall" component={VideoCallScreen} />
         <Tab.Screen name="Chat" component={ChatScreen} />
