534657bff469bbe22143dfd0e8995f7dc7468238
abhishek0ghill0000-ai <abhishek0ghill0000@gmail.com>
2026-01-21 14:32:37 +0530

    Fix server folder

--- server/src/routes/callRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getAgoraToken } from '../controllers/callController';
-import { agoraTokenLimiter, asyncHandler } from '../middlewares';
-
-const router = Router();
-
-router.get('/token', agoraTokenLimiter, asyncHandler(getAgoraToken));
-
-export default router;
